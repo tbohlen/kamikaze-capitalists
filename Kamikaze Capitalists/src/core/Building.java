91cc1b92250eb18fa88d9665ee0432601dec7139
Todd Layton <tlayton@mit.edu>
2012-09-19 16:32:44 -0400

    fixed formatting

--- Kamikaze Capitalists/src/core/Building.java
+++ Kamikaze Capitalists/src/core/Building.java
@@ -1,23 +1,23 @@
 package core;
 
 public class Building {
-    
-    public int height;
-    
-    public final Player owner;
-    
-    public final boolean isCapital;
-    
-    public Building(Player owner){
-	height = 1;
-	this.owner = owner;
-	isCapital = false;
-    }
-    
-    public Building(Player owner, boolean isCapital){
-	height = 1;
-	this.owner = owner;
-	this.isCapital = isCapital;
-    }
-    
+
+	public int height;
+
+	public final Player owner;
+
+	public final boolean isCapital;
+
+	public Building(Player owner) {
+		height = 1;
+		this.owner = owner;
+		isCapital = false;
+	}
+
+	public Building(Player owner, boolean isCapital) {
+		height = 1;
+		this.owner = owner;
+		this.isCapital = isCapital;
+	}
+
 }
--- Kamikaze Capitalists/src/core/Player.java
+++ Kamikaze Capitalists/src/core/Player.java
@@ -1,13 +1,13 @@
 package core;
 
 public class Player {
-    
-    public final String name;
-    
-    public int actionCount;
-    
-    public Player(String name){
-	this.name = name;
-	actionCount = 0;
-    }
+
+	public final String name;
+
+	public int actionCount;
+
+	public Player(String name) {
+		this.name = name;
+		actionCount = 0;
+	}
 }
