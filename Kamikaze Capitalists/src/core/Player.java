9405d5a22203444cafb64c77e9fb2c5813620146
Todd Layton <tlayton@mit.edu>
2012-09-19 15:30:46 -0400

    added LWJGL to project

---
+++ Kamikaze Capitalists/src/core/Player.java
@@ -0,0 +1,13 @@
+package core;
+
+public class Player {
+    
+    public final String name;
+    
+    public int actionCount;
+    
+    public Player(String name){
+	this.name = name;
+	actionCount = 0;
+    }
+}
